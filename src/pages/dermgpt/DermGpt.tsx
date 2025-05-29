c1695b44e276d76d24a2ec18ab799d7f8d21f716
noah <ntcampbell03@gmail.com>
2025-05-29 16:09:23 -0700

    change iframe

--- src/pages/dermgpt/DermGpt.tsx
+++ src/pages/dermgpt/DermGpt.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./DermGpt.css";
 
 const DermGpt: React.FC = () => {
-    const url = "https://app-backend-dfbx76tgwktxa.azurewebsites.net/#/";
+    const url = "https://capps-backend-46rde5d4cdkug.gentlegrass-bcb11f25.eastus2.azurecontainerapps.io/";
 
     return (
         <div className="iframe-container">
