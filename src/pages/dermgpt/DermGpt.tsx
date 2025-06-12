9d496df935617f8d4ea209af0a6ddadc511d3c95
noah <ntcampbell03@gmail.com>
2025-06-11 22:44:43 -0700

    add loading behind iframe

--- src/pages/dermgpt/DermGpt.tsx
+++ src/pages/dermgpt/DermGpt.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DermGpt.css";
 
 const DermGpt: React.FC = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const url = "https://capps-backend-46rde5d4cdkug.gentlegrass-bcb11f25.eastus2.azurecontainerapps.io/";
+
+    const handleIframeLoad = () => {
+        setIsLoading(false);
+    };
 
     return (
         <div className="iframe-container">
-            <iframe src={url} title="Embedded Frame" />
+            {isLoading && (
+                <div className="loading-message">
+                    Loading...
+                </div>
+            )}
+            <iframe
+                src={url}
+                title="Embedded Frame"
+                onLoad={handleIframeLoad}
+                style={{ display: isLoading ? 'none' : 'block' }}
+            />
         </div>
     );
 };
